10cacd8923ecb37fa8693ced6362aac1dd1da826
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-26 09:05:18 -0400

    If word if lowercase and longer than 4 characters puts that

--- exercise.rb
+++ exercise.rb
@@ -1,1 +1,11 @@
-["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+words = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+def check_words(words)
+  words.each do |word|
+    if word.length > 4 && word == word.downcase
+      "long and lowercase"
+    end
+  end
+end
+
+puts check_words(words)
